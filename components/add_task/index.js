51ededd61d0f481660e9026cdda031dd8a36553f
Beksultan <beksultan.sagnaev@gmail.com>
2022-09-13 15:28:32 +0600

    feat: add task and change chekboxes

---
+++ components/add_task/index.js
@@ -0,0 +1,68 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import modal from '../../styles/Modal.module.css'
+import style from '../../styles/Home.module.css'
+import Link from 'next/link';
+// import data from '../../data.json'
+import { useState } from 'react';
+
+export default function Addtask({options, stage, setStage, setModalAddTask, inputList, setInputList, switchTheme, handleAddTask, taskTitle, setTaskTitle, taskDescription, setTaskDescription}) {
+    
+    
+    
+    const handleInputChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...inputList];
+        list[index][name] = value;
+        setInputList(list);
+      };
+       
+      // handle click event of the Remove button
+      const handleRemoveClick = index => {
+        const list = [...inputList];
+        list.splice(index, 1);
+        setInputList(list);
+      };
+       
+      // handle click event of the Add button
+      const handleAddClick = () => {
+        setInputList([...inputList, { title: "", isCompleted: false }]);
+      };
+
+  return (
+    <div className={modal.modal} onClick={() => setModalAddTask(false)}>
+        <div className={modal.modalcontent} onClick={e => e.stopPropagation()}>
+            <div>Title</div>
+            <input type="text" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)}/>
+            <div>Description</div>
+            <textarea name="" id="" cols="30" rows="10" value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)}></textarea>
+            <div>Subtasks</div>
+            {inputList.map((x, i) => {
+        return (
+          <div className="box">
+            <input
+              name="title"
+              value={x.firstName}
+              onChange={e => handleInputChange(e, i)}
+            />
+            <div className="btn-box">
+              {inputList.length !== 1 && <button onClick={() => handleRemoveClick(i)}>Remove</button>}
+              {inputList.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+            </div>
+          </div>
+        );
+      })}
+            <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
+            <button>+ Add New Subtask</button>
+            <div>Status</div>
+            
+            <select name="" id="" value={stage} onChange={(e) => setStage(e.target.value)}>
+            {options.map((option) =>
+                <option value={option.value}>{option.value}</option>
+                )}
+            </select>
+            <button onClick={handleAddTask}>Create Task</button>
+        </div>
+    </div>
+  )
+}
---
+++ components/view_task/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import modal from '../../styles/Modal.module.css'
+import style from '../../styles/Home.module.css'
+import Link from 'next/link';
+// import data from '../../data.json'
+import { useState } from 'react';
+
+export default function Viewtask({handleSubtaskCompleted, options, setModalOpened, selectedBoard, selectedTitle, switchTheme, data, sidebarOpened}) {
+
+  return (
+    <div className={modal.modal} onClick={() => setModalOpened(false)}>
+      <div className={modal.modalcontent} onClick={e => e.stopPropagation()}>
+        {data.boards.filter((board) => board.name === selectedBoard)[0].columns.map((col) => col.tasks.filter((task) => task.title === selectedTitle).map((item) => (
+          <div>
+            <div className={style.task_title}>{item.title}</div>
+            <div className={modal.task_subtitle}>{item.description}</div>
+            <div className={style.task_subtitle}>{item.subtasks.filter((subtask) => subtask.isCompleted === true).length} of {item.subtasks.length} subtasks</div>
+            <div className={modal.subtask_list}>
+            {item.subtasks.map((subtask) => (
+              <div>
+                {subtask.isCompleted && <div className={modal.subtask}><input type="checkbox" checked onClick={() => handleSubtaskCompleted(col.name, item.title, subtask.title)}/> <div className={modal.done_subtask}>{subtask.title}</div></div>}
+                {!subtask.isCompleted && <div className={modal.subtask}><input type="checkbox" onClick={() => handleSubtaskCompleted(col.name, item.title, subtask.title)}/> <div className={modal.undone_subtask}>{subtask.title}</div></div>}
+              </div>
+            ))}
+            </div>
+            
+                <select name="" id="">
+                <option value={item.status}>{item.status}</option>
+                {options.map(option => (
+                <>
+                {item.status !== option.value && <option value={option.value}>{option.value}</option>}
+                </>
+                ))}
+              </select>
+                
+            
+          </div>
+        )))}
+      </div>
+    </div>
+  )
+}
